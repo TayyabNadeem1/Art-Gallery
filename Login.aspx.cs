107c4f655e530f1881a9551b3f44e0d4d15dc0be
Tayyab Nadeem <103959510+TayyabNadeem1@users.noreply.github.com>
2023-04-25 21:56:27 +0500

    Add files via upload

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -1,17 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace DB_Project
+
+namespace Dexterite
 {
-    public partial class Home : System.Web.UI.Page
+    public partial class Login : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void validateLogin(object sender, EventArgs e)
+        {
+            string Name = txtName.Text;
+            string Password = TxtPassword.Text;
+            SqlConnection con = new SqlConnection("Server=DESKTOP-7C763KG\\SQLEXPRESS01; Database=dexterite;Trusted_Connection=True");
+            SqlCommand cmd = new SqlCommand("select * from Users where Name=@name and Password=@password", con);
+            cmd.Parameters.AddWithValue("@name", Name);
+            cmd.Parameters.AddWithValue("@password", Password);
+            SqlDataAdapter d = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            d.Fill(ds, "Users");
+            if (ds.Tables["Users"].Rows.Count == 0)
+            {
+                Response.Write("Invalid User");
+            }
+            else
+            {
+
+                Response.Redirect("Home.aspx");
+            }
         }
     }
 }
--- Signup.aspx.cs
+++ Signup.aspx.cs
@@ -4,8 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Text;
+using System.Windows;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Services.Description;
 
-namespace DB_Project
+namespace Dexterite
 {
     public partial class Signup : System.Web.UI.Page
     {
@@ -13,5 +19,59 @@ namespace DB_Project
         {
 
         }
+        SqlConnection con = new SqlConnection("Server=DESKTOP-7C763KG\\SQLEXPRESS01; Database=dexterite;Trusted_Connection=True");
+        protected void SignupCheck(object sender, EventArgs e)
+        {
+            
+                if (txtName.Text != "" && TxtEmail.Text != "" && TxtPassword.Text != "" && TxtConfirm.Text != "")
+                {
+                    if (TxtPassword.Text == TxtConfirm.Text)
+                    {
+                        int v = check(TxtEmail.Text);
+                        if (v != 1)
+                        {
+                            con.Open();
+                            SqlCommand command = new SqlCommand("insert into [Users] values(@name,@password,@email,null,null,null)", con);
+                            command.Parameters.AddWithValue("@name", txtName.Text);
+                            command.Parameters.AddWithValue("@email", TxtEmail.Text);
+                            command.Parameters.AddWithValue("@password", TxtPassword.Text);
+                            
+                            command.ExecuteNonQuery();
+                            con.Close();
+                            txtName.Text = "";
+                            TxtPassword.Text = "";
+                            TxtEmail.Text = "";
+                            TxtConfirm.Text = "";
+                            Response.Redirect("Home.aspx");
+                        }
+                        else
+                        {
+                            Response.Write("You already have an account with this email !");
+                        }
+                    }
+                    else
+                    {
+                        Response.Write("Password doesn't match");
+                    }
+            }
+            else
+            {
+                Response.Write("Fill all the options");
+            }
+
+            
+        }
+        int check(string email)
+        {
+            con.Open();
+            string query = "select count(*) from [Users] where email='" + email + "'";
+            SqlCommand command = new SqlCommand(query, con);
+            int v = (int)command.ExecuteScalar();
+            con.Close();
+            return v;
+        }
+
+
     }
 }
+    
