aac22240bb1865b5312bc409da72ab18bd25e284
Tayyab Nadeem <103959510+TayyabNadeem1@users.noreply.github.com>
2023-04-27 02:17:20 +0500

    Add files via upload

--- Home.aspx.cs
+++ Home.aspx.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
-namespace DB_Project
+using System.Data;
+namespace Dexterite
 {
-    public partial class Home1 : System.Web.UI.Page
+    public partial class Home : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+        SqlConnection con = new SqlConnection("Server=DESKTOP-7C763KG\\SQLEXPRESS01; Database=dexterite;Trusted_Connection=True");
+        protected void search_box(object sender, EventArgs e)
+        {
+            string find = "select * from [Users] where (name like '%'+ @Name+ '%')";
+            SqlCommand cmd = new SqlCommand(find, con);
+            cmd.Parameters.Add("@Name", SqlDbType.NVarChar).Value = searchingName.Text;
+            cmd.ExecuteNonQuery();
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            da.Fill(ds, "name");
+            
 
         }
     }
