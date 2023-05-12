e3a1495d68cbc881fe55d9b0e2059a843038fcb7
mabdx <102393129+mabdx@users.noreply.github.com>
2023-05-12 10:46:51 +0500

    Add files via upload

---
+++ ArtistPage.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace dexterite4
+{
+    public partial class ArtistPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlConnection con = new SqlConnection();
+            SqlDataAdapter sda = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            if (Session["Username"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                con.ConnectionString = "Server=ABDULLAH\\SQLEXPRESS; Database=dexterite;Trusted_Connection=True";
+                con.Open();
+                cmd.CommandText = "select * from users where name= '" + Session["Username"] + "'";
+                cmd.Connection = con;
+                sda.SelectCommand = cmd;
+                sda.Fill(ds);
+                Label1.Text = ds.Tables[0].Rows[0]["Name"].ToString();
+
+                cmd.CommandText = "select picture from users where name= '" + Session["Username"] + "'";
+                cmd.Connection = con;
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        byte[] imgd = (byte[])dr["picture"];
+                        string image = Convert.ToBase64String(imgd, 0, imgd.Length);
+                        Image1.ImageUrl = "data:image/png;base64," + image;
+                    }
+                }
+                dr.Close();
+
+                cmd.CommandText = "select artpicture from art inner join likedart on art.artid=likedart.ArtID inner join users on likedart.userid=users.userid where users.name='" + Session["Username"] + "'";
+                cmd.Connection = con;
+                SqlDataReader dr3 = cmd.ExecuteReader();
+                if (dr3.HasRows)
+                {
+                    while (dr3.Read())
+                    {
+                        byte[] imgd = (byte[])dr3["artpicture"];
+                        string image2 = Convert.ToBase64String(imgd, 0, imgd.Length);
+                        Image2.ImageUrl = "data:image/png;base64," + image2;
+                    }
+                }
+            }
+        }
+    }
+}
