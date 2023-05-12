a7f7dc46d2264172246426fe1d8baa72f50c2c7c
mabdx <102393129+mabdx@users.noreply.github.com>
2023-05-12 10:47:39 +0500

    Update Login.aspx.cs

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Data;
@@ -8,7 +8,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 
-namespace Dexterite
+namespace dexterite4
 {
     public partial class Login : System.Web.UI.Page
     {
@@ -19,23 +19,41 @@ namespace Dexterite
 
         protected void validateLogin(object sender, EventArgs e)
         {
-            string Name = txtName.Text;
-            string Password = TxtPassword.Text;
-            SqlConnection con = new SqlConnection("Server=DESKTOP-7C763KG\\SQLEXPRESS01; Database=dexterite;Trusted_Connection=True");
+            string username = txtName.Text;
+            string password = TxtPassword.Text;
+            SqlConnection con = new SqlConnection("Server=ABDULLAH\\SQLEXPRESS; Database=dexterite;Trusted_Connection=True");
             SqlCommand cmd = new SqlCommand("select * from Users where Name=@name and Password=@password", con);
-            cmd.Parameters.AddWithValue("@name", Name);
-            cmd.Parameters.AddWithValue("@password", Password);
+            cmd.Parameters.AddWithValue("@name", username);
+            cmd.Parameters.AddWithValue("@password", password);
             SqlDataAdapter d = new SqlDataAdapter(cmd);
             DataSet ds = new DataSet();
             d.Fill(ds, "Users");
             if (ds.Tables["Users"].Rows.Count == 0)
             {
-                Response.Write("Invalid User");
+                SqlCommand cmd2 = new SqlCommand("select * from artists where Name=@name and Password=@password", con);
+                cmd2.Parameters.AddWithValue("@name", username);
+                cmd2.Parameters.AddWithValue("@password", password);
+                SqlDataAdapter d1 = new SqlDataAdapter(cmd2);
+                DataSet ds2 = new DataSet();
+                d1.Fill(ds2, "Artists");
+                if (ds2.Tables["Artists"].Rows.Count != 0)
+                {
+                    string name1 = txtName.Text;
+                    Session["Username"] = name1;
+                    Response.Redirect("UserPage.aspx");
+                }
+                else
+                {
+
+                    Response.Write("Invalied Details");
+                }
             }
             else
             {
+                string name = txtName.Text;
+                Session["Username"] = name;
+                Response.Redirect("ArtistPage.aspx");
 
-                Response.Redirect("Home.aspx");
             }
         }
     }
